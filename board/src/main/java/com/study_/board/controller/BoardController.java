b00fd89828672142b4fb48fead6a877e6f3dc602
김승찬 <s24041@gsm.hs.kr>
2024-08-26 20:20:28 +0900

    spring-board

--- board/src/main/java/com/study_/board/controller/BoardController.java
+++ board/src/main/java/com/study_/board/controller/BoardController.java
@@ -1,12 +1,19 @@
 package com.study_.board.controller;
 
 import com.study_.board.entity.Board;
+import com.study_.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
     @GetMapping("/board/write")
     public String boardwriteForm(){
 
@@ -17,8 +24,16 @@ public class BoardController {
     @PostMapping("/board/writepro")
     public String boardwritePro(Board board) {
 
-        System.out.println(board.getTitle());
+        boardService.write(board);
 
         return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+
+        model.addAttribute("list", boardService.boardList());
+
+        return "boardlist";
     }
 }
--- board/src/main/java/com/study_/board/service/BoardService.java
+++ board/src/main/java/com/study_/board/service/BoardService.java
@@ -1,14 +1,24 @@
 package com.study_.board.service;
 
+import com.study_.board.entity.Board;
 import com.study_.board.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BoardService {
 
+    @Autowired
     private BoardRepository boardRepository;
 
-    public void write() {
+    public void write(Board board) {
 
+        boardRepository.save(board);
+    }
+    public List<Board> boardList() {
+
+        return boardRepository.findAll();
     }
 }
